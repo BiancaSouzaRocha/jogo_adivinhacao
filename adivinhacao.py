5af82cc04eaba28799fea331030e2103a308175b
Bianca Rocha <biancasouzarocha@outlook.com>
2022-08-16 16:39:11 -0300

    Incluindo random

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,22 +1,32 @@
+import random
+
+
 print("#################################")
 print("Bem vindo ao Jogo de Adivinhação!")
 print("#################################")
+numero_secreto = random.randrange(1, 50)
+total_tentativas = 3
 
-chute_str = input("Digite um número: ")
-chute = int(chute_str)
+for rodada in range (1, total_tentativas + 1):
+    print(f"Tentativa {rodada} de {total_tentativas}")
+    chute_str = input("Digite um número entre 1 e 50: ")
+    chute = int(chute_str)
 
-print(f"Você digitou {chute}")
+    if (chute < 1 or chute > 50):
+        print("Você deve digitar um número entre 1 e 50.")
+        continue
 
-numero_secreto = 42
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-if (chute):
-    print("Você acertou!")
-else:
-    if (maior):
-        print("Você errou! O seu chute foi maior do que o número secreto")
-    elif (menor):
-        print("Você errou! O seu chute foi maior do que o número secreto")
-print ("Fim do jogo!")
+    if (acertou):
+        print("Você acertou!")
+        break
+    else:
+        if (maior):
+            print("Você errou! O seu chute foi maior do que o número secreto")
+        elif (menor):
+            print("Você errou! O seu chute foi menor do que o número secreto")
+
+print("Fim de Jogo!")
